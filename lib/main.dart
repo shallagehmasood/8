30d56f8d5858a3a77270db8670a05c059085b3fd
shallagehmasood <shallagehmasood@gmail.com>
2025-10-13 16:02:38 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,84 +1,28 @@
-import 'dart:convert';
-import 'dart:typed_data';
 import 'package:flutter/material.dart';
-import 'package:web_socket_channel/io.dart';
+import 'package:websocketchannel/websocketchannel.dart';
+import 'dart:convert';
 
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: ImageViewer(),
-  ));
-}
+void main() => runApp(MyApp());
 
-final channel = IOWebSocketChannel.connect('ws://178.63.171.244:5000');
-
-class ImageViewer extends StatefulWidget {
-  @override
-  _ImageViewerState createState() => _ImageViewerState();
-}
-
-class _ImageViewerState extends State<ImageViewer> {
-  List<String> imageList = [];
-  Uint8List? imageData;
-  bool isLoading = false;
-
-  @override
-  void initState() {
-    super.initState();
-    channel.stream.listen((message) {
-      try {
-        final data = jsonDecode(message);
-        if (data['type'] == 'list') {
-          setState(() {
-            imageList = List<String>.from(data['images']);
-          });
-        } else if (data['type'] == 'image') {
-          setState(() {
-            imageData = base64Decode(data['data']);
-            isLoading = false;
-          });
-        }
-      } catch (e) {
-        print('خطا در دریافت داده: $e');
-      }
-    });
-  }
-
-  void requestImage(String name) {
-    setState(() {
-      isLoading = true;
-      imageData = null;
-    });
-    channel.sink.add(jsonEncode({"request": name}));
-  }
+class MyApp extends StatelessWidget {
+  final channel = WebSocketChannel.connect(
+    Uri.parse('ws://178.63.171.244:5000'), // آدرس سرور شما
+  );
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("تصاویر دریافتی")),
-      body: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          children: [
-            if (imageList.isNotEmpty)
-              Wrap(
-                spacing: 8,
-                runSpacing: 8,
-                children: imageList.map((name) {
-                  return ElevatedButton(
-                    onPressed: () => requestImage(name),
-                    child: Text(name),
-                  );
-                }).toList(),
-              ),
-            SizedBox(height: 20),
-            if (isLoading)
-              Center(child: CircularProgressIndicator()),
-            if (imageData != null)
-              Expanded(
-                child: Image.memory(imageData!),
-              ),
-          ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('ارسال پیام به سرور')),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              final message = jsonEncode({"message": "hello"});
+              channel.sink.add(message);
+              print("📤 Sent: $message");
+            },
+            child: Text("ارسال پیام"),
+          ),
         ),
       ),
     );
