2d4602b7f6f06f274592c71801e80b7cbd77fde8
shallagehmasood <shallagehmasood@gmail.com>
2025-10-13 19:58:51 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
-import 'dart:convert';
-import 'dart:typed_data';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final WebSocketChannel channel = WebSocketChannel.connect(
-    Uri.parse('ws://178.63.171.244:5000'),
+  final channel = WebSocketChannel.connect(
+    Uri.parse(
+      'ws://178.63.171.244:5000',
+    ), // اگر روی موبایل تست می‌کنی، آدرس IP سرور رو جایگزین کن
   );
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: WebSocketDemo(channel: channel),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class WebSocketDemo extends StatefulWidget {
-  final WebSocketChannel channel;
-
-  WebSocketDemo({required this.channel});
-
-  @override
-  _WebSocketDemoState createState() => _WebSocketDemoState();
-}
-
-class _WebSocketDemoState extends State<WebSocketDemo> {
-  Uint8List? imageData;
-
-  void sendRequest() {
-    final message = jsonEncode({"request": "hello.png"});
-    widget.channel.sink.add(message);
-    print("📤 Sent: $message");
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    widget.channel.stream.listen((data) {
-      final decoded = jsonDecode(data);
-      if (decoded['type'] == 'image') {
-        setState(() {
-          imageData = base64Decode(decoded['data']);
-        });
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('ارتباط با سرور')),
-      body: Column(
-        children: [
-          ElevatedButton(
-            onPressed: sendRequest,
-            child: Text("درخواست تصویر"),
+      home: Scaffold(
+        appBar: AppBar(title: Text('اتصال WebSocket')),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              StreamBuilder(
+                stream: channel.stream,
+                builder: (context, snapshot) {
+                  return Text(
+                    snapshot.hasData
+                        ? 'پیام دریافتی: ${snapshot.data}'
+                        : 'در انتظار پیام...',
+                  );
+                },
+              ),
+              TextField(
+                onSubmitted: (text) {
+                  channel.sink.add(text);
+                },
+                decoration: InputDecoration(labelText: 'ارسال پیام'),
+              ),
+            ],
           ),
-          if (imageData != null)
-            Expanded(child: Image.memory(imageData!)),
-        ],
+        ),
       ),
     );
   }
