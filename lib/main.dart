effb43caf082815c8e5eab79808d0e90dae80399
shallagehmasood <shallagehmasood@gmail.com>
2025-10-13 14:19:11 +0330

    اولین ارسال پروژه

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,57 @@
+import 'dart:convert';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:web_socket_channel/io.dart';
-import 'dart:typed_data';
 
-void main() => runApp(MyApp());
+final channel = IOWebSocketChannel.connect('ws://178.63.171.244:5000');
 
-class MyApp extends StatelessWidget {
-  final channel = IOWebSocketChannel.connect('ws://178.63.171.244:5000');
+class ImageViewer extends StatefulWidget {
+  @override
+  _ImageViewerState createState() => _ImageViewerState();
+}
+
+class _ImageViewerState extends State<ImageViewer> {
+  List<String> imageList = [];
+  Uint8List? imageData;
+
+  @override
+  void initState() {
+    super.initState();
+    channel.stream.listen((message) {
+      final data = jsonDecode(message);
+      if (data['type'] == 'list') {
+        setState(() {
+          imageList = List<String>.from(data['images']);
+        });
+      } else if (data['type'] == 'image') {
+        setState(() {
+          imageData = base64Decode(data['data']);
+        });
+      }
+    });
+  }
+
+  void requestImage(String name) {
+    channel.sink.add(jsonEncode({"request": name}));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('تصویر از VPS')),
-        body: StreamBuilder(
-          stream: channel.stream,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              final bytes = snapshot.data as Uint8List;
-              return Image.memory(bytes);
-            } else {
-              return Center(child: Text('در انتظار تصویر...'));
-            }
-          },
-        ),
+    return Scaffold(
+      appBar: AppBar(title: Text("تصاویر دریافتی")),
+      body: Column(
+        children: [
+          if (imageList.isNotEmpty)
+            Wrap(
+              children: imageList.map((name) {
+                return ElevatedButton(
+                  onPressed: () => requestImage(name),
+                  child: Text(name),
+                );
+              }).toList(),
+            ),
+          if (imageData != null) Expanded(child: Image.memory(imageData!)),
+        ],
       ),
     );
   }
