42f86ad6e59aee8a31da1bf2ab1b713434ae04e3
shallagehmasood <shallagehmasood@gmail.com>
2025-10-13 22:52:09 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,64 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(BitcoinLiveApp());
 
-class MyApp extends StatelessWidget {
-  final channel = WebSocketChannel.connect(
-    Uri.parse(
-      'ws://178.63.171.244:5000',
-    ), // اگر روی موبایل تست می‌کنی، آدرس IP سرور رو جایگزین کن
-  );
-
+class BitcoinLiveApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('اتصال WebSocket')),
-        body: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            children: [
-              StreamBuilder(
-                stream: channel.stream,
-                builder: (context, snapshot) {
-                  return Text(
-                    snapshot.hasData
-                        ? 'پیام دریافتی: ${snapshot.data}'
-                        : 'در انتظار پیام...',
-                  );
-                },
-              ),
-              TextField(
-                onSubmitted: (text) {
-                  channel.sink.add(text);
-                },
-                decoration: InputDecoration(labelText: 'ارسال پیام'),
-              ),
-            ],
-          ),
+      title: 'قیمت زنده بیت‌کوین',
+      theme: ThemeData(primarySwatch: Colors.orange),
+      home: BitcoinPriceScreen(),
+    );
+  }
+}
+
+class BitcoinPriceScreen extends StatefulWidget {
+  @override
+  _BitcoinPriceScreenState createState() => _BitcoinPriceScreenState();
+}
+
+class _BitcoinPriceScreenState extends State<BitcoinPriceScreen> {
+  late WebSocketChannel channel;
+  String price = 'در حال دریافت...';
+
+  @override
+  void initState() {
+    super.initState();
+    channel = WebSocketChannel.connect(
+      Uri.parse('wss://stream.binance.com:9443/ws/btcusdt@trade'),
+    );
+
+    channel.stream.listen((message) {
+      final data = json.decode(message);
+      final newPrice = double.parse(data['p']).toStringAsFixed(2);
+      setState(() {
+        price = '\$ $newPrice';
+      });
+    }, onError: (error) {
+      setState(() {
+        price = 'خطا در اتصال';
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    channel.sink.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('قیمت زنده بیت‌کوین')),
+      body: Center(
+        child: Text(
+          'قیمت BTC/USDT:\n$price',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
         ),
       ),
     );
